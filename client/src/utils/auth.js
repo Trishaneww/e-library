1394bec27db7c248d0d5b49e45524a1940d0763b
Trishane <trishaneww@gmail.com>
2022-11-25 02:11:13 -0800

    auth.js for authentication

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -7,19 +7,18 @@ class AuthService {
 
   loggedIn() {
     const token = this.getToken();
-    // If there is a token and it's not expired, return `true`
+    // Returns true if there token is yet to be expired
     return token && !this.isTokenExpired(token) ? true : false;
   }
 
   isTokenExpired(token) {
-    // Decode the token to get its expiration time that was set by the server
     const decoded = decode(token);
-    // If the expiration time is less than the current time (in seconds), the token is expired and we return `true`
+    // Returns true if the expiration time is less than the current time
     if (decoded.exp < Date.now()/1000) {
       localStorage.removeItem('id_token');
       return true;
     }
-    // If token hasn't passed its expiration time, return `false`
+    // Returns false if token passes expiration time
     return false;
   }
 
